eb95adbcd73780f4f04ca6d4418bc36b1a40fb3b
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-01 11:32:15 -0400

    added contact section

--- js/navbar.js
+++ js/navbar.js
@@ -4,3 +4,26 @@ const navElement = document.querySelector("nav");
 toggleButton.addEventListener("click", function(){
     navElement.classList.toggle("activated");
 })
+
+const portfolioSection = document.querySelector("#portfolio");
+const aboutSection = document.querySelector("#about");
+const contactSection = document.querySelector("#contact");
+
+const portfolioButton = document.querySelector(".navbar-portfolio");
+const aboutButton = document.querySelector(".navbar-about");
+const contactButton = document.querySelector(".navbar-contact");
+
+portfolioButton.addEventListener("click", function() {
+    window.scrollTo(0, portfolioSection.offsetTop - 100);
+    navElement.className = "";
+});
+
+aboutButton.addEventListener("click", function() {
+    window.scrollTo(0, aboutSection.offsetTop - 100);
+    navElement.className = "";
+});
+
+contactButton.addEventListener("click", function() {
+    window.scrollTo(0, contactSection.offsetTop - 100);
+    navElement.className = "";
+});
